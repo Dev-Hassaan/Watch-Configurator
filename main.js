9219a8cca4c410dd93da9015b5444c69ad6580a1
Hassaan <syedhassaan720@gmail.com>
2024-10-27 11:55:10 +0500

    v.0.5 Accordions Added
    Some Js code is being commented, Old tab-content divs are moved to the Accordion Body

--- main.js
+++ main.js
@@ -9,34 +9,34 @@ $(".watch-isActive").on("click", function() {
     $(this).addClass("active")
 })
 
-var currentIndex = $(".tab-content").length - $(".tab-content").length
-$(".tab-content").eq(currentIndex).show()
-$("#pre_tab").on("click", function() {
+// var currentIndex = $(".tab-content").length - $(".tab-content").length
+// $(".tab-content").eq(currentIndex).show()
+// $("#pre_tab").on("click", function() {
 
-    $(".tab-content").hide()
+//     $(".tab-content").hide()
     
-    currentIndex += 1
+//     currentIndex += 1
 
-    if (currentIndex < 0) {
-        currentIndex = $(".tab-content").length - 1 
-    } else if ( currentIndex > $(".tab-content").length - 1) {
-        currentIndex = 0
-    }
+//     if (currentIndex < 0) {
+//         currentIndex = $(".tab-content").length - 1 
+//     } else if ( currentIndex > $(".tab-content").length - 1) {
+//         currentIndex = 0
+//     }
 
-    $(".tab-content").eq(currentIndex).show()
+//     $(".tab-content").eq(currentIndex).show()
 
-})
-$("#next_tab").on("click", function() {
-    $(".tab-content").hide()
+// })
+// $("#next_tab").on("click", function() {
+//     $(".tab-content").hide()
     
-    currentIndex -= 1
+//     currentIndex -= 1
 
-    if (currentIndex < 0) {
-        currentIndex = $(".tab-content").length - 1 
-    } else if ( currentIndex > $(".tab-content").length - 1) {
-        currentIndex = 0
-    }
+//     if (currentIndex < 0) {
+//         currentIndex = $(".tab-content").length - 1 
+//     } else if ( currentIndex > $(".tab-content").length - 1) {
+//         currentIndex = 0
+//     }
 
-    $(".tab-content").eq(currentIndex).show()
+//     $(".tab-content").eq(currentIndex).show()
 
-})
+// })
