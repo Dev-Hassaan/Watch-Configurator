941ea1738cb90ff735cb7df43a928d10e360c491
Hassaan <syedhassaan720@gmail.com>
2024-12-06 18:45:43 +0500

    v.1.3 New Corrections

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import * as bootstrap from 'bootstrap'
 import SnedPostMessage from './src/js/PostMessage'
 import StateHandler from './src/js/StateHandler'
     
+var app = document.getElementById("app")
 
 StateHandler(".watch-icon-1", "click")
 StateHandler(".watch-icon-2", "click")
@@ -13,12 +14,21 @@ StateHandler(".watch-icon-3", "click")
 StateHandler(".watch-icon-4", "click")
 StateHandler(".watch-icon-5", "click")
 StateHandler(".watch-icon-6", "click")
-StateHandler(".watch-icon-7", "click")
-StateHandler(".watch-icon-8", "click")
+// StateHandler(".watch-icon-7", "click")
+// StateHandler(".watch-icon-8", "click")
 StateHandler(".watch-icon-9", "click")
 StateHandler(".watch-icon-10", "click")
-StateHandler(".watch-icon-11", "click")
+// StateHandler(".watch-icon-11", "click")
 // StateHandler(".watch-icon-12", "click")
+
+// Get the Index of the accordion which is active/shown
+document.querySelectorAll('.accordion-collapse').forEach((collapse, index) => {
+    collapse.addEventListener('shown.bs.collapse', function () {
+      SnedPostMessage(app, `Active-Scene:${index}`)
+    });
+  });
+  
+  
 
 $(".watch-ui").hide()
 window.addEventListener('message', (event) => {
